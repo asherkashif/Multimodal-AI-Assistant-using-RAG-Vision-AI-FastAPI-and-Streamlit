64b44c406086fca0ae6d0da2c07c90b19739e3a8
Asher Kashif <asherkashif26@gmail.com>
2026-07-14 13:33:25 +0500

    d

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,5 +1,3 @@
-from pydoc import text
-
 from fastapi import APIRouter, UploadFile, File
 from app.services.pdf_service import extract_text_from_pdf
 from app.services.chunk_service import split_text
@@ -7,28 +5,36 @@ from app.services.vector_store import create_vector_store
 import os
 import shutil
 
-
 router = APIRouter()
+
 upload_directory = "uploads"
 os.makedirs(upload_directory, exist_ok=True)
 
-chunks = split_text(text)
-create_vector_store(chunks)
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(upload_directory, file.filename)
+
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
     if file.filename.endswith(".pdf"):
+        # Extract text
         extracted_text = extract_text_from_pdf(file_path)
+
+        # Split text into chunks
+        chunks = split_text(extracted_text)
+
+        # Create FAISS vector store
+        create_vector_store(chunks)
+
         return {
             "filename": file.filename,
-            "text": extracted_text[:3000]  # Return only the first 3000 characters of the extracted text
+            "chunks": len(chunks),
+            "message": "Vector store created successfully."
         }
-    
+
     return {
         "filename": file.filename,
-        "chunks": len(chunks),
-        "message": "File uploaded successfully"
+        "message": "Only PDF files are supported."
     }
--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,7 +1,9 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS 
+from dotenv import load_dotenv
+load_dotenv()
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
 
 def create_vector_store(chunks):
     vector_db = FAISS.from_texts(chunks, embeddings)
